582bed5e29f8fab2574afeb3a05ef5c6226c0a4f
alunny <alunny@gmail.com>
2012-02-01 12:09:29 -0800

    [writer] handle multiple sections

--- test/pbxWriter.js
+++ test/pbxWriter.js
@@ -2,45 +2,32 @@ var pbx = require('../lib/pbxProject'),
     fs = require('fs'),
     myProj;
 
-function testProjectContents(filename) {
+function testProjectContents(filename, test) {
+    var myProj = new pbx(filename),
+        content = fs.readFileSync(filename, 'utf-8');
 
+    // normalize tabs vs strings
+    content = content.replace(/\t/g, '    ');
+
+    myProj.parse(function (err, projHash) {
+        var written = myProj.writeSync();
+
+        test.equal(content, written);
+        test.done();
+    });
 }
 
 exports.writeSync = {
-    'should write out the simple "hash" test': function (test) {
-        var file = 'test/parser/projects/hash.pbxproj',
-            myProj = new pbx(file),
-            contents = fs.readFileSync(file, 'utf-8');
-
-        myProj.parse(function (err, projHash) {
-            var written = myProj.writeSync();
-
-            test.equal(written, contents);
-            test.done();
-        })
+    'should write out the "hash" test': function (test) {
+        testProjectContents('test/parser/projects/hash.pbxproj', test);
     },
-    'should write out the simple "with_array" test': function (test) {
-        var file = 'test/parser/projects/with_array.pbxproj',
-            myProj = new pbx(file),
-            contents = fs.readFileSync(file, 'utf-8');
-
-        myProj.parse(function (err, projHash) {
-            var written = myProj.writeSync();
-
-            test.equal(written, contents);
-            test.done();
-        })
+    'should write out the "with_array" test': function (test) {
+        testProjectContents('test/parser/projects/with_array.pbxproj', test);
     },
-    'should write out the simple "section" test': function (test) {
-        var file = 'test/parser/projects/section.pbxproj',
-            myProj = new pbx(file),
-            contents = fs.readFileSync(file, 'utf-8');
-
-        myProj.parse(function (err, projHash) {
-            var written = myProj.writeSync();
-
-            test.equal(written, contents);
-            test.done();
-        })
+    'should write out the "section" test': function (test) {
+        testProjectContents('test/parser/projects/section.pbxproj', test);
+    },
+    'should write out the "two-sections" test': function (test) {
+        testProjectContents('test/parser/projects/two-sections.pbxproj', test);
     }
 }
